42df0fbdd70868541aaf51f89f8afe78e10ee5e1
SerjWeesp <serj.amarin@gmail.com>
2024-10-20 19:25:13 +0200

    Added __init__.py and __main__.py to initialize the package

---
+++ amarinml/__main__.py
@@ -0,0 +1,6 @@
+from .AmarinML import *
+
+if __name__ == "__main__":
+    # Example usage
+    # Call your functions here if you want to test them directly
+    print("Running AmarinML package directly.")
